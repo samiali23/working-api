c1b8cc69e22ca93ce67daa64f7c997a884705bb7
samiali23 <54128872+samiali23@users.noreply.github.com>
2022-03-20 10:00:14 -0700

    working api

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+let rappers = {
+    '21 savage':{
+    'age': 28,
+    'birthName': 'Sheyaa Bin Abraham-Jospeh',
+    'birthLocation': 'London, England'
+    },
+    'chance the rapper':{
+    'age': 29,
+    'birthName': 'Abraham-Jospeh Sheyaa Bin',
+    'birthLocation': 'England,London'
+    },
+    'unknown':{
+        'age': 28,
+        'birthName': 'nono',
+        'birthLocation': 'nono'
+        }
+}
+
+app.get('/', (request,response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/rappers/:rapperName', (request,response) => {
+    const rapName = request.params.rapperName.toLowerCase()
+    console.log(rapName)
+    if(rappers[rapName]){
+        response.json(rappers[rapName])
+    }else{
+        response.json(rappers['unknown'])
+    }
+    response.json(rappers[rapName])
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+}) 
